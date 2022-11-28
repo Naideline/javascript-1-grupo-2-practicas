8f36f04fa4e32a378dc3e955f5d98314b189d482
Naideline <107892053+Naideline@users.noreply.github.com>
2022-11-28 17:53:42 -0600

    Add files via upload
    quizz

---
+++ main.js
@@ -0,0 +1,76 @@
+{
+    "data" [
+    {
+    "id": 7,
+    "email": "michael.lawson@reqres.in",
+    "age": 30,
+    "userAccount": "Premium",
+    "first_name": "Michael",
+    "last_name": "Lawson",
+    "description": "Lorem ipsum"
+    },
+    {
+    "id": 8,
+    "email": "lindsay.ferguson@reqres.in",
+    "age": 78,
+    "userAccount": "Basic",
+    "first_name": "Lindsay",
+    "last_name": "Ferguson",
+    "description": "Lorem ipsum"
+    },
+    {
+    "id": 9,
+    "email": "tobias.funke@reqres.in",
+    "age": 23,
+    "userAccount": "Basic",
+    "first_name": "Tobias",
+    "last_name": "Funke",
+    "description": "Lorem ipsum"
+    },
+    {
+    "id": 10,
+    "email": "byron.fields@reqres.in",
+    "age": 86,
+    "userAccount": "Basic",
+    "first_name": "Byron",
+    "last_name": "Fields",
+    "description": "Lorem ipsum"
+    },
+    {
+    "id": 11,
+    "email": "george.edwards@reqres.in",
+    "age": 30,
+    "userAccount": "Premium",
+    "first_name": "George",
+    "last_name": "Edwards",
+    "description": "Lorem ipsum"
+    },
+    {
+    "id": 12,
+    "email": "rachel.howell@reqres.in",
+    "age": 67,
+    "userAccount": "Premium",
+    "first_name": "Rachel",
+    "last_name": "Howell",
+    "description": "Lorem ipsum"
+    },
+    {
+    "id": 13,
+    "email": "adrian.bell@reqres.in",
+    "age": 54,
+    "userAccount": "Basic",
+    "first_name": "Adrian",
+    "last_name": "Bell",
+    "description": "Lorem ipsum"
+    },
+    {
+    "id": 14,
+    "email": "adrian.bell@reqres.in",
+    "age": 54,
+    "userAccount": "",
+    "first_name": "Adrian",
+    "last_name": "Bell",
+    "description": "Lorem ipsum"
+    }
+    ]
+    }
